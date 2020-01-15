ce73cb18ef1d03525281847b8121f89cae13a8b7
romanDj <true-rock99@ya.ru>
2020-01-15 22:16:14 +0300

    fix forms

--- code/geopaparazzi/projects/models.py
+++ code/geopaparazzi/projects/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from geopaparazzi.users.models import User
 from django.urls import reverse
@@ -7,6 +8,7 @@ class Subdivision(models.Model):
     title = models.CharField("название подразделения", max_length=70)
     description = models.CharField("краткое описание", max_length=300)
     participants = models.ManyToManyField(User, verbose_name='участники')
+    created_date = models.DateTimeField(default=datetime.datetime.now(), verbose_name='Дата создания')
 
     def __str__(self):
         return self.title
@@ -21,9 +23,11 @@ class Subdivision(models.Model):
 #Модель проектов
 class Project(models.Model):
     title = models.CharField("название проекта", max_length=70)
-    description = models.CharField("краткое описание", max_length=30)
+    description = models.CharField("краткое описание", max_length=300)
     owner = models.ForeignKey(Subdivision, on_delete=models.CASCADE, verbose_name='подразделение')
-
+    created_date = models.DateTimeField(default=datetime.datetime.now(), verbose_name='Дата создания')
+    edit_date = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         verbose_name = 'Проект'
         verbose_name_plural = 'Проекты'
